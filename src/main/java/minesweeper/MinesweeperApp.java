5cb3ea476efb3f17b4206939bfc9f8a342b2c31f
HN05 <henrik.nordvik@gmail.com>
2025-03-28 17:49:43 +0100

    fixed small img bug

--- src/main/java/minesweeper/MinesweeperApp.java
+++ src/main/java/minesweeper/MinesweeperApp.java
@@ -3,9 +3,11 @@ package minesweeper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class MinesweeperApp extends Application {
 
@@ -13,10 +15,19 @@ public class MinesweeperApp extends Application {
         Application.launch(args);
     }
 
+    private URL getResource(final String name) {
+        return getClass().getResource(name);
+    }
+
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Minesweeper");
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("App.fxml"))));
+        final Image appIcon = new Image(getResource("minesweeper.png").toExternalForm());
+        // set app icon (does not set dock icon)
+        primaryStage.getIcons().add(appIcon);
+        primaryStage.setMinWidth(300);
+        primaryStage.setMinHeight(300);
+        primaryStage.setScene(new Scene(FXMLLoader.load(getResource("App.fxml"))));
         primaryStage.show();
     }
 
