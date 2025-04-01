a87ead77374097815d4c605b36d8042f5adbbc89
HN05 <henrik.nordvik@gmail.com>
2025-04-01 23:34:12 +0200

    temp commit

--- src/main/java/minesweeper/MinesweeperController.java
+++ src/main/java/minesweeper/MinesweeperController.java
@@ -2,6 +2,7 @@ package minesweeper;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import minesweeper.model.Board;
 import minesweeper.model.BoardGenerator;
@@ -11,19 +12,37 @@ import minesweeper.model.GameListener;
 
 public class MinesweeperController implements GameListener {
 
-    private Game game = new Game(new Board(BoardGenerator.generateCells((short) 12, (short) 12, 12)));
-    private boolean isMarking = false;
+	private Game game = new Game(new Board(BoardGenerator.generateCells((short) 12, (short) 12, 12)));
+	private boolean isMarking = false;
 
-    @FXML
-    private GridPane grid;
+	@FXML
+	private GridPane grid;
 
-    @FXML
-    private Label bombCounter; 
+	@FXML
+	private Label bombCounter;
 
-    @FXML
-    private void handleToggleFlagMode() {
-        isMarking = !isMarking;
-    }
+	@FXML
+	private void handleToggleFlagMode() {
+		isMarking = !isMarking;
+	}
+
+	@FXML
+	private void initialize() {
+		final Board board = game.getBoard();
+		for (int y = 0; y < board.getRowCount(); y++) {
+			for (int x = 0; x < board.getColCount(); x++) {
+				final Button button = new Button();
+				button.setPrefSize(50, 50);
+				final int x_loc = x; final int y_loc = y;
+				button.setOnAction(e -> handleClick(x_loc, y_loc));
+				grid.add(button, x, y);
+			}
+		}
+	}
+
+	private void handleClick(final int x, final int y) {
+
+	}
 
 	@Override
 	public void updatedCell(final Cell cell) {
