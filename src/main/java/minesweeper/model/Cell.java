77f7ed42833702cc28107440a369a7a910f2abe3
HN05 <henrik.nordvik@gmail.com>
2025-04-09 23:56:15 +0200

    formatting

--- src/main/java/minesweeper/model/Cell.java
+++ src/main/java/minesweeper/model/Cell.java
@@ -1,66 +1,66 @@
 package minesweeper.model;
 
 public final class Cell {
-    private final int x;
-    private final int y;
-    private final boolean isBomb;
-    private int nearbyBombs = 0; // calculated later
-    private boolean isRevealed = false;
-    private boolean isMarked = false;
+	private final int x;
+	private final int y;
+	private final boolean isBomb;
+	private int nearbyBombs = 0; // calculated later
+	private boolean isRevealed = false;
+	private boolean isMarked = false;
 
-    public Cell(final int x, final int y, final boolean isBomb) {
-        this.x = x;
-        this.y = y;
-        this.isBomb = isBomb;
-    }
+	public Cell(final int x, final int y, final boolean isBomb) {
+		this.x = x;
+		this.y = y;
+		this.isBomb = isBomb;
+	}
 
-    // Should only be used by BoardGenerator class
-    void incrementNearbyBombs() {
-        nearbyBombs++;
-    }
+	// Should only be used by BoardGenerator class
+	void incrementNearbyBombs() {
+		nearbyBombs++;
+	}
 
-    // Should only be used by Game class
-    void revealCell() {
+	// Should only be used by Game class
+	void revealCell() {
 		if (isMarked) {
 			throw new IllegalStateException("Can't reveal cell that is marked");
 		}
-        isRevealed = true;
-    }
+		isRevealed = true;
+	}
 
-    // Should only be used by Game class
-    void markCell() {
-        isMarked = true;
-    }
+	// Should only be used by Game class
+	void markCell() {
+		isMarked = true;
+	}
 
-    // Should only be used by Game class
+	// Should only be used by Game class
 	void unMarkCell() {
 		isMarked = false;
 	}
 
-    public int getX() {
-        return x;
-    }
+	public int getX() {
+		return x;
+	}
 
-    public int getY() {
-        return y;
-    }
+	public int getY() {
+		return y;
+	}
 
-    public boolean isBomb() {
-        return isBomb;
-    }
+	public boolean isBomb() {
+		return isBomb;
+	}
 
-    public int getNearbyBombs() {
-        return nearbyBombs;
-    }
+	public int getNearbyBombs() {
+		return nearbyBombs;
+	}
 
-    public boolean isRevealed() {
-        return isRevealed;
-    }
+	public boolean isRevealed() {
+		return isRevealed;
+	}
 
-    public boolean isMarked() {
-        return isMarked;
-    }
-	
+	public boolean isMarked() {
+		return isMarked;
+	}
+
 	public void reset() {
 		isMarked = false;
 		isRevealed = false;
